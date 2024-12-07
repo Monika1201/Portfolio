fa619635af218b045bebf2bd01157ea228229f3b
Monika1201 <150769553+Monika1201@users.noreply.github.com>
2024-12-07 18:25:07 +0530

    deleted unwanted files

--- assets/sample.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.profile img').addEventListener('mouseenter', () => {
-    document.querySelector('.profile img').classList.add('animate');
-});
-
-document.querySelector('.profile img').addEventListener('mouseleave', () => {
-    document.querySelector('.profile img').classList.remove('animate');
-});
-
